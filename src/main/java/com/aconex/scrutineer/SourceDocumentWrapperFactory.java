6adbd6d5813ace10e89eecb5274a815a93f6655b
Claudio Melendrez <claudio.melendrez@globant.com>
2016-08-29 17:39:34 -0300

    Move shade plugin to profile, remove useless comments

--- src/main/java/com/aconex/scrutineer/SourceDocumentWrapperFactory.java
+++ src/main/java/com/aconex/scrutineer/SourceDocumentWrapperFactory.java
@@ -2,9 +2,6 @@ package com.aconex.scrutineer;
 
 import org.elasticsearch.search.SearchHit;
 
-/**
- * Created by claudio.melendrez on 25/08/16.
- */
 public enum SourceDocumentWrapperFactory implements DocumentWrapperFactory {
     INSTANCE;
 
